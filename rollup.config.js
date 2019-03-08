af6a4f2162d9291f38d6f26bd8c81b88ef932bbd
Dierk Koenig <dierk.koenig@canoo.com>
2019-03-08 15:36:52 +0100

    cleaning the alltests generation

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,50 @@
-const fs = require('fs');
+// using node to generate an all.tests.js file
+// by scanning the directory structure recursively for test files
+
+const fs   = require('fs');
 const path = require('path');
 
 const configs = [];
 
-const dirsToCheck = ['huerto', 'puerro', 'research', 'test'];
+const dirsToCheck  = ['huerto', 'puerro', 'research', 'test'];
 const allTestsFile = 'all.tests.js';
-const testsEnding = '.test.js';
-const entryPoints = ['script.js', testsEnding, allTestsFile];
-const name = '_';
-const format = 'iife';
+const testsEnding  = '.test.js';
+const entryPoints  = ['script.js', testsEnding, allTestsFile];
+const name         = '_';
+const format       = 'iife';
 
 const bundleOutputName = file => `dist/${delFileEnding(file)}.bundle.js`;
-const delFileEnding = s => s.replace(/\.[^/.]+$/, '');
+const delFileEnding    = name => name.replace(/\.[^/.]+$/, '');  // delete from last dot after the last slash until end
 
-const createConfig = (d, f) => ({
-  input: path.join(d, f),
+const createConfig = (dir, filename) => ({
+  input:  path.join(dir, filename),
   output: {
-    file: path.join(d, bundleOutputName(f)),
+    file: path.join(dir, bundleOutputName(filename)),
     name,
     format,
   },
 });
 
-function fillConfigsFromFile(dir) {
+const fillConfigsFromFile = dir => {
   const dirContent = fs.readdirSync(dir, { withFileTypes: true });
 
   dirContent
-    .filter(c => c.isDirectory())
-    .forEach(d => fillConfigsFromFile(path.join(dir, d.name)));
+    .filter (d => d.isDirectory())
+    .forEach(d => fillConfigsFromFile(path.join(dir, d.name))); // recursive descent into subdirs DFS
 
   dirContent
-    .filter(c => c.isFile())
-    .filter(f => entryPoints.some(e => f.name.endsWith(e)))
+    .filter (f => f.isFile())
+    .filter (f => entryPoints.some(e => f.name.endsWith(e)))
     .forEach(f => configs.push(createConfig(dir, f.name)));
-}
+};
 
 dirsToCheck.forEach(fillConfigsFromFile);
 
 let testImports = `// Generated file\n\n`;
 testImports += configs
   .filter(c => c.input.endsWith(testsEnding))
-  .map(c => `import '../${c.input}';\n`)
-  .join('');
+  .map   (c => `import '../${c.input}';\n`)
+  .join  ('');
 
 console.log(configs);
 
