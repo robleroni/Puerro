6617bc96f557199af63b6e0973773a6c2ef9aaa3
Robin Christen <robin.christen@students.fhnw.ch>
2019-05-10 11:28:49 +0200

    working on ObservableObject

--- puerro/observable/observable.js
+++ puerro/observable/observable.js
@@ -4,15 +4,11 @@
  * @module observable
  */
 
-export { Observable, EventManager, ObservableList, ObservableObject };
+export { Observable, ObservableObject, ObservableList };
 
 const Observable = item => {
   const listeners = [];
   return {
-    onChange: callback => {
-      listeners.push(callback);
-      callback(item, item);
-    },
     get: () => item,
     set: newItem => {
       if (item === newItem) return;
@@ -20,68 +16,47 @@ const Observable = item => {
       item = newItem;
       listeners.forEach(notify => notify(newItem, oldItem));
     },
+    onChange: callback => {
+      listeners.push(callback);
+      callback(item, item);
+    },
   };
 };
 
 const ObservableObject = object => {
-  const listeners = [];
-  const events = {};
+  const listeners   = [];
+  const subscribers = {};
+
+  const notify = newObject => {
+    if (object == newObject) return;
+    const oldObject = object;
+    object = newObject;
+
+    Object.keys(newObject).forEach(key => {
+      const newValue = newObject[key];
+      const oldValue = oldObject[key];
+      if (oldValue === newValue) return;
+      (subscribers[key] || []).forEach(subscriber => subscriber(newValue, oldValue));
+    });
+    listeners.forEach(listener => listener(newObject, oldObject));
+  };
+
   return {
-    get: () => object,
-    set: data => {
-      object = {...object, ...data};
-      Object.keys(data).forEach(key => {
-        const handlers = events[key] || [];
-        handlers.forEach(handler => handler(data[key]));
-      })
-      listeners.forEach(listener => listener(data));
-    },
+    get: ()            => object,
+    set: newObject     => notify({ ...object, ...newObject }),
+    push: (key, value) => notify({ ...object, ...{ [key]: value } }),
+    remove: key        => notify({ ...object, ...{ [key]: undefined } }),
+    replace: newObject => notify(newObject),
     onChange: callback => {
       listeners.push(callback);
       callback(object, object);
     },
-    subscribe: (key, handler) => {
-      events[key] = events[key] || [];
-      events[key].push(handler);
-    }
-  };
-};
-
-const EventManager = () => {
-  const events = {};
-  return {
-    publish: (name, data) => {
-      const handlers = events[name] || [];
-      handlers.forEach(handler => handler(data));
+    subscribe: (key, callback) => {
+      subscribers[key] = subscribers[key] || [];
+      subscribers[key].push(callback);
+      callback(object[key], object[key]);
     },
-    subscribe: (name, handler) => {
-      events[name] = events[name] || [];
-      events[name].push(handler);
-    },
-    unsubscribe: (name, hanlder) => {
-      events[name] = (events[name] || []).filter(h => h !== hanlder);
-    },
-  };
-};
-
-const Observer = callback => {
-  return {
-    observe: observable => observable.onChange(callback),
-  };
-};
-
-const EventObservable = obj => {
-  const events = { CHANGED: 0, ADDED: 1, REMOVED: 2, MADE_INVALID: 3 };
-  const observers = [];
-  return {
-    events,
-    get: () => obj,
-    onChange: observer => observers.push(observer),
-    changeTo: (newObj, event = events.CHANGED) => {
-      if (obj === newObj) return;
-      observers.forEach(notify => notify(newObj, event, obj));
-      obj = newObj;
-    },
+    // unsubscribe, removeOnChange
   };
 };
 
@@ -120,6 +95,42 @@ const ObservableList = list => {
     indexOf: item => list.indexOf(item),
     get: index => list[index],
     getAll: () => list,
+  };
+};
+
+/* EXPERIMENTS */
+const EventManager = () => {
+  const subscribers = {};
+  return {
+    publish:   (name, data)       => (subscribers[name] || []).forEach(subscriber => subscriber(data)),
+    subscribe: (name, subscriber) => {
+      subscribers[name] = subscribers[name] || [];
+      subscribers[name].push(subscriber);
+    },
+    unsubscribe: (name, subscriber) => {
+      subscribers[name] = (subscribers[name] || []).filter(s => s !== subscriber);
+    },
+  };
+};
+
+const Observer = callback => {
+  return {
+    observe: observable => observable.onChange(callback),
+  };
+};
+
+const EventObservable = obj => {
+  const events = { CHANGED: 0, ADDED: 1, REMOVED: 2, MADE_INVALID: 3 };
+  const observers = [];
+  return {
+    events,
+    get: () => obj,
+    onChange: observer => observers.push(observer),
+    changeTo: (newObj, event = events.CHANGED) => {
+      if (obj === newObj) return;
+      observers.forEach(notify => notify(newObj, event, obj));
+      obj = newObj;
+    },
   };
 };
 
